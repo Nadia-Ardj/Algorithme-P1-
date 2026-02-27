e5d15eda0c3bf67ff2e21bd624facaf2fe4e38c1
Nadia-Ardj <nadia.ardjoune.08@gmail.com>
2026-02-28 00:52:22 +0100

    Add files via upload

---
+++ 2.4 Racines réelles d’une équation du second degré.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std ;
+
+int main(){
+
+    int a , b , c , delta=0;
+    float x1 , x2 ;
+
+    cout<<"entrer la valeur des coefficients  :  ";
+    cin>>a>>b>>c;
+    delta = (b*b) - 4*(a*c);
+
+    if(delta==0){
+        x1= b/(2*a);
+        cout<<x1<<endl;
+    }
+    else if(delta>0){
+        x1=( -b-sqrt(delta))/2*a;
+          x2=( -b+sqrt(delta))/2*a;
+          cout<<x1<<x2<<endl;
+    }
+    else{
+        cout<<"pas de solution "<<endl;
+    }
+
+  return 0;
+}
+
